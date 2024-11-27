2a2c06c873d7325950a106640115f7d63c301d46
Osvaldo-arq <oswald.aulasi@gmail.com>
2024-11-27 18:35:10 -0300

    arquivos
    trabalho Escala

---
+++ front/escalas/src/config/AppTheme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const AppTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+    background: {
+      default: '#ffffff',
+    },
+  },
+});
+
+export default AppTheme;
